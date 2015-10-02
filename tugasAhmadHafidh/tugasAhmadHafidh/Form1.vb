c1e15cabc1968f3acd873d3e200ec8d8732e9165
ahmadhafidh <ahmad.hafidhayatullah@gmail.com>
2015-10-02 16:41:10 +0700

    Tugas 1 ganti background color
    pengumpulan tugas pertama

---
+++ tugasAhmadHafidh/tugasAhmadHafidh/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Me.BackColor = Color.DarkBlue Then
+            Me.BackColor = Color.Red
+        ElseIf Me.BackColor = Color.Red Then
+            Me.BackColor = Color.DarkBlue
+
+        End If
+    End Sub
+End Class
